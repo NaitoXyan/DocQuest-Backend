0f12603e3996211a1e9633646a50769bf1c8a430
Chalbert Reyes <125259061+NaitoXyan@users.noreply.github.com>
2024-08-14 00:31:43 +0800

    added auth (incomplete)

---
+++ docquest/docquest/views.py
@@ -0,0 +1,14 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def login(request):
+    return Response({})
+
+@api_view(['POST'])
+def signup(request):
+    return Response({})
+
+@api_view(['GET'])
+def test_token(request):
+    return Response({})
