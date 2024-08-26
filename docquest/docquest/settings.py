61eec639ba02c47eb9457f82bb2536690c988085
Chalbert Reyes <125259061+NaitoXyan@users.noreply.github.com>
2024-08-26 21:31:30 +0800

    stuffs

--- docquest/docquest/settings.py
+++ docquest/docquest/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,14 +80,9 @@ WSGI_APPLICATION = 'docquest.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'docquestdb',
-        'USER': 'postgres',
-        'PASSWORD': 'docquestpass4',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('postgresql://postgres:HQpoqtWGqWhfLsVlGNCGLEbpVhBIRvSU@junction.proxy.rlwy.net:39692/railway')
+    )
 }
 
 
