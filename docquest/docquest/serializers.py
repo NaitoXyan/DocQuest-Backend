6f37045189752b0e16b845456d5c1b53b02b70ef
NaitoXyan <chalbertareyes@gmail.com>
2024-10-23 19:51:28 +0800

    changes

--- docquest/docquest/serializers.py
+++ docquest/docquest/serializers.py
@@ -34,11 +34,6 @@ class UserLoginSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = CustomUser
         fields = ['userID', 'firstname', 'lastname', 'roles']
-
-class TargetGroupSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = TargetGroup
-        fields = ['targetGroup']
     
 class GoalsAndObjectivesSerializer(serializers.ModelSerializer):
     class Meta(object):
@@ -61,7 +56,7 @@ class EvaluationAndMonitoringSerializer(serializers.ModelSerializer):
             'risksAssumptions', 'type'
         ]
 
-class BudgetaryRequirementsItemsSerializer(serializers.ModelSerializer):
+class BudgetRequirementsItemsSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = BudgetRequirementsItems
         fields = ['itemName', 'ustpAmount', 'partnerAmount', 'totalAmount']
@@ -72,6 +67,11 @@ class ProjectActivitiesSerializer(serializers.ModelSerializer):
         fields = [
             'objective', 'involved', 'targetDate', 'personResponsible'
         ]
+
+class ProjectManagementTeamSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = ProjectManagementTeam
+        fields = ['name']
 
 class LoadingOfTrainersSerializer(serializers.ModelSerializer):
     class Meta(object):
@@ -177,11 +177,11 @@ class GetProjectSerializer(serializers.ModelSerializer):
     userID = GetProjectLeaderSerializer()
     projectLocationID = AddressSerializer()
     agency = PartnerAgencySerializer(many=True)
-    targetGroups = TargetGroupSerializer(source='targetGroup', many=True)
+    
     goalsAndObjectives = GoalsAndObjectivesSerializer(many=True)
     monitoringPlanSchedules = MonitoringPlanAndScheduleSerializer(source='monitoringPlanSched', many=True)
     evaluationAndMonitorings = EvaluationAndMonitoringSerializer(source='evalAndMonitoring', many=True)
-    budgetaryRequirements = BudgetaryRequirementsItemsSerializer(source='budgetRequirements', many=True)
+    budgetRequirements = BudgetRequirementsItemsSerializer(source='budgetRequirements', many=True)
     projectActivities = ProjectActivitiesSerializer(many=True)
     loadingOfTrainers = LoadingOfTrainersSerializer(many=True)
     signatories = SignatoriesSerializer(source='signatoryProject', many=True)
@@ -194,84 +194,81 @@ class GetProjectSerializer(serializers.ModelSerializer):
             'projectCategory', 'researchTitle', 'program', 'accreditationLevel',
             'college', 'projectLocationID', 'agency', 'targetImplementation',
             'totalHours', 'background', 'projectComponent', 'beneficiaries',
-            'totalBudget', 'targetGroups', 'goalsAndObjectives', 'monitoringPlanSchedules',
-            'evaluationAndMonitorings', 'budgetaryRequirements', 'projectActivities',
+            'totalBudget', 'goalsAndObjectives', 'monitoringPlanSchedules',
+            'evaluationAndMonitorings', 'budgetRequirements', 'projectActivities',
             'loadingOfTrainers', 'signatories', 'proponents'
         ]
 
 class PostProjectSerializer(serializers.ModelSerializer):
     userID = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
-    agency = serializers.PrimaryKeyRelatedField(queryset=PartnerAgency.objects.all(), many=True)
-
+    proponents = ProponentsSerializer(many=True)
     projectLocationID = PostAddressSerializer()
-    targetGroups = TargetGroupSerializer(many=True)
+    agency = serializers.PrimaryKeyRelatedField(queryset=PartnerAgency.objects.all(), many=True)
     goalsAndObjectives = GoalsAndObjectivesSerializer(many=True)
-    monitoringPlanSchedules = MonitoringPlanAndScheduleSerializer(many=True)
-    evaluationAndMonitorings = EvaluationAndMonitoringSerializer(many=True)
-    budgetaryRequirements = BudgetaryRequirementsItemsSerializer(many=True)
     projectActivities = ProjectActivitiesSerializer(many=True)
+    projectManagementTeam = ProjectManagementTeamSerializer(many=True)
+    budgetRequirements = BudgetRequirementsItemsSerializer(many=True)
+    evaluationAndMonitorings = EvaluationAndMonitoringSerializer(many=True)
+    monitoringPlanSchedules = MonitoringPlanAndScheduleSerializer(many=True)
+    
     loadingOfTrainers = LoadingOfTrainersSerializer(many=True)
     signatories = SignatoriesSerializer(many=True)
-    proponents = ProponentsSerializer(many=True)
 
     class Meta(object):
         model = Project
         fields = [
             'userID', 'programCategory', 'projectTitle', 'projectType',
-            'projectCategory', 'researchTitle', 'program', 'accreditationLevel',
-            'college', 'projectLocationID', 'agency', 'targetImplementation',
-            'totalHours', 'background', 'projectComponent', 'beneficiaries',
-            'totalBudget', 'targetGroups', 'goalsAndObjectives', 'monitoringPlanSchedules',
-            'evaluationAndMonitorings', 'budgetaryRequirements', 'projectActivities',
-            'loadingOfTrainers', 'signatories', 'proponents'
+            'projectCategory', 'researchTitle', 'program', 'accreditationLevel', 'college', 'beneficiaries',  
+            'targetImplementation', 'totalHours', 'background', 'projectComponent', 'targetScope',
+            'ustpBudget', 'partnerAgencyBudget', 'totalBudget', 'proponents', 'projectLocationID',
+            'agency', 'goalsAndObjectives', 'projectActivities', 'projectManagementTeam', 'budgetRequirements',
+            'evaluationAndMonitorings', 'monitoringPlanSchedules', 'loadingOfTrainers', 'signatories', 
         ]
 
     def create(self, validated_data):
-        address_data = validated_data.pop('projectLocation')
+        proponents_data = validated_data.pop('proponents')
+        address_data = validated_data.pop('projectLocationID')
         projectLocationID = Address.objects.create(**address_data)
-
-        targetGroups_data = validated_data.pop('targetGroups')
+        agency_data = validated_data.pop('agency')
         goalsAndObjectives_data = validated_data.pop('goalsAndObjectives')
-        monitoringPlanSchedules_data = validated_data.pop('monitoringPlanSchedules')
-        evaluationAndMonitorings_data = validated_data.pop('evaluationAndMonitorings')
-        budgetaryRequirements_data = validated_data.pop('budgetaryRequirements')
         projectActivities_data = validated_data.pop('projectActivities')
+        projectManagementTeam_data = validated_data.pop('projectManagementTeam')
+        budgetRequirements_data = validated_data.pop('budgetRequirements')
+        evaluationAndMonitorings_data = validated_data.pop('evaluationAndMonitorings')
+        monitoringPlanSchedules_data = validated_data.pop('monitoringPlanSchedules')
         loadingOfTrainers_data = validated_data.pop('loadingOfTrainers')
         signatories_data = validated_data.pop('signatories')
-        proponents_data = validated_data.pop('proponents')
-
-        agency_data = validated_data.pop('agency')
-
+        
         project = Project.objects.create(projectLocationID=projectLocationID, **validated_data)
 
         project.agency.set(agency_data)
 
-        for targetGroup_data in targetGroups_data:
-            TargetGroup.objects.create(project=project, **targetGroup_data)
+        for proponent_data in proponents_data:
+            Proponents.objects.create(project=project, **proponent_data)
         
         for goalsAndObjective_data in goalsAndObjectives_data:
             GoalsAndObjectives.objects.create(project=project, **goalsAndObjective_data)
+        
+        for projectActivity_data in projectActivities_data:
+            ProjectActivities.objects.create(project=project, **projectActivity_data)
 
-        for monitoringPlanSchedule_data in monitoringPlanSchedules_data:
-            MonitoringPlanAndSchedule.objects.create(project=project, **monitoringPlanSchedule_data)
+        for projectManagementTeam_data in projectManagementTeam_data:
+            ProjectManagementTeam.objects.create(project=project, **projectManagementTeam_data)
+
+        for budgetaryRequirement_data in budgetRequirements_data:
+            BudgetRequirementsItems.objects.create(project=project, **budgetaryRequirement_data)
 
         for evaluationAndMonitoring_data in evaluationAndMonitorings_data:
             EvaluationAndMonitoring.objects.create(project=project, **evaluationAndMonitoring_data)
 
-        for budgetaryRequirement_data in budgetaryRequirements_data:
-            BudgetRequirementsItems.objects.create(project=project, **budgetaryRequirement_data)
-
-        for projectActivity_data in projectActivities_data:
-            ProjectActivities.objects.create(project=project, **projectActivity_data)
+        for monitoringPlanSchedule_data in monitoringPlanSchedules_data:
+            MonitoringPlanAndSchedule.objects.create(project=project, **monitoringPlanSchedule_data)
 
         for loadingOfTrainer_data in loadingOfTrainers_data:
             LoadingOfTrainers.objects.create(project=project, **loadingOfTrainer_data)
 
         for signatory_data in signatories_data:
             Signatories.objects.create(project=project, **signatory_data)
-
-        for proponent_data in proponents_data:
-            Proponents.objects.create(project=project, **proponent_data)
 
         return project
 
--- docquest/docquestapp/models.py
+++ docquest/docquestapp/models.py
@@ -5,6 +5,7 @@ from .managers import CustomUserManager
 from django.utils import timezone
 from django.contrib.auth.models import PermissionsMixin
 import datetime
+import hashlib
 
 class Roles(models.Model):
     roleID = models.AutoField(primary_key=True)
@@ -101,27 +102,32 @@ class Project(models.Model):
 
     projectID = models.AutoField(primary_key=True)
     userID = models.ForeignKey(CustomUser, related_name='projectUser', on_delete=models.CASCADE)
-    programCategory = models.CharField(max_length=50)
-    projectTitle = models.CharField(max_length=150)
-    projectType = models.CharField(max_length=50)
-    projectCategory = models.CharField(max_length=100)
-    researchTitle = models.CharField(max_length=150)
-    program = models.CharField(max_length=150)
-    accreditationLevel = models.CharField(max_length=50)
-    college = models.CharField(max_length=50)
-    projectLocationID = models.ForeignKey(Address, related_name='projectLocation', on_delete=models.CASCADE) 
-    agency = models.ManyToManyField(PartnerAgency, related_name='projectAgency')
-    targetImplementation = models.DateField()
-    totalHours = models.FloatField()
-    background = models.TextField()
-    projectComponent = models.TextField()
-    beneficiaries = models.TextField()
-    totalBudget = models.IntegerField()
+    programCategory = models.CharField(max_length=50) #1
+    projectTitle = models.CharField(max_length=150) #2
+    projectType = models.CharField(max_length=50) #3 
+    projectCategory = models.CharField(max_length=100) #4
+    researchTitle = models.CharField(max_length=150) #5
+    program = models.CharField(max_length=150) #6
+    accreditationLevel = models.CharField(max_length=50) #7
+    college = models.CharField(max_length=50) #8
+    beneficiaries = models.TextField() #9
+    targetImplementation = models.DateField() #10
+    totalHours = models.FloatField() #11
+    background = models.TextField() #12
+    projectComponent = models.TextField() #13
+    targetScope = models.TextField() #14
+    ustpBudget = models.IntegerField() #15
+    partnerAgencyBudget = models.IntegerField() #16
+    totalBudget = models.IntegerField() #17
+    projectLocationID = models.ForeignKey(Address, related_name='projectLocation', on_delete=models.CASCADE) #a2
     moaID = models.ForeignKey(MOA, related_name='projectMoa', on_delete=models.CASCADE, null=True)
+    agency = models.ManyToManyField(PartnerAgency, related_name='projectAgency') #a3
+
+    routingProgress = models.IntegerField(default=0)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
     dateCreated = models.DateTimeField(auto_now_add=True)
 
-    uniqueCode = models.CharField(max_length=50, unique=True, blank=True, null=True)
+    uniqueCode = models.CharField(max_length=255, unique=True, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         # Ensure the object is saved first so we have a projectID and date_created
@@ -132,23 +138,72 @@ class Project(models.Model):
         if not self.uniqueCode:
             # Create unique_code from pk and date_created
             self.uniqueCode = f"{self.pk}-{self.dateCreated.strftime('%Y%m%d')}"
-            super().save(*args, **kwargs)
+        
+        # Set project status based on routingProgress
+        if self.routingProgress >= 7:
+            self.status = 'approved'
+        elif self.status != 'rejected':  # Do not change to pending if already rejected
+            self.status = 'pending'
+
+        super().save(*args, **kwargs)
+
 
 class Signatories(models.Model):
+    APPROVAL_CHOICES = [
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+        ('none', 'None'),
+    ]
+    
     project = models.ForeignKey(Project, related_name='signatoryProject', on_delete=models.CASCADE)
     userID = models.ForeignKey(CustomUser, related_name='signatoryUser', on_delete=models.CASCADE)
-    approvalStatus = models.BooleanField(default=False)
+    signatureCode = models.CharField(max_length=255)
+    approvalStatus = models.CharField(max_length=10, choices=APPROVAL_CHOICES, default='none')
 
-class Proponents(models.Model):
+    def generate_signature_code(self):
+        """
+        Generate the signature code in the format roleId-userId-projectId-date+checksum.
+        """
+        date_str = self.project.dateCreated.strftime('%Y%m%d')  # Convert dateCreated to YYYYMMDD
+        code_without_checksum = f"{self.roleID}-{self.userID.pk}-{self.project.projectID}-{date_str}"
+        checksum = self.calculate_checksum(code_without_checksum)
+        return f"{code_without_checksum}+{checksum}"
+
+    def calculate_checksum(self, code):
+        """
+        Calculate a checksum using a simple hash (or sum of ASCII values).
+        """
+        return sum(ord(char) for char in code) % 10  # Simple checksum (mod 10)
+
+    def save(self, *args, **kwargs):
+        # Generate the signatureCode based on the required format
+        if not self.signatureCode:
+            self.signatureCode = self.generate_signature_code()
+
+        # Handle approval status logic
+        if self.approvalStatus == 'approved' and not self.pk:
+            # Increment the routingProgress of the associated project
+            self.project.routingProgress += 1
+
+        elif self.approvalStatus == 'rejected':
+            # If rejected, set project status to 'rejected'
+            self.project.status = 'rejected'
+            # Ensure routingProgress doesn't get updated further once rejected
+            self.project.routingProgress = min(self.project.routingProgress, 7)
+
+        # Save the project with the updated routingProgress and status
+        self.project.save()
+
+        super(Signatories, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.signatureCode
+
+class Proponents(models.Model): #a1
     project = models.ForeignKey(Project, related_name='proponent', on_delete=models.CASCADE)
     proponent = models.CharField(max_length=50)
 
-class TargetGroup(models.Model):
-    targetGroupID = models.AutoField(primary_key=True)
-    targetGroup = models.CharField(max_length=200)
-    project = models.ForeignKey(Project, related_name='targetGroup', on_delete=models.CASCADE)
-
-class GoalsAndObjectives(models.Model):
+class GoalsAndObjectives(models.Model): #a5
     GAOID = models.AutoField(primary_key=True)
     goalsAndObjectives = models.TextField()
     project = models.ForeignKey(Project, related_name='goalsAndObjectives', on_delete=models.CASCADE)
@@ -163,7 +218,7 @@ class LoadingOfTrainers(models.Model):
     totalBudgetRequirement = models.IntegerField()
     project = models.ForeignKey(Project, related_name='loadingOfTrainers', on_delete=models.CASCADE)
 
-class ProjectActivities(models.Model):
+class ProjectActivities(models.Model): #a6
     projectActivitiesID = models.AutoField(primary_key=True)
     objective = models.TextField()
     involved = models.TextField()
@@ -171,7 +226,11 @@ class ProjectActivities(models.Model):
     personResponsible = models.CharField(max_length=50)
     project = models.ForeignKey(Project, related_name='projectActivities', on_delete=models.CASCADE)
 
-class BudgetRequirementsItems(models.Model):
+class ProjectManagementTeam(models.Model): #a7
+    name = models.CharField(max_length=50)
+    project = models.ForeignKey(Project, related_name='projectManagementTeam', on_delete=models.CASCADE)
+
+class BudgetRequirementsItems(models.Model): #a8
     itemID = models.AutoField(primary_key=True)
     itemName = models.CharField(max_length=50)
     ustpAmount = models.IntegerField()
@@ -179,7 +238,7 @@ class BudgetRequirementsItems(models.Model):
     totalAmount = models.IntegerField()
     project = models.ForeignKey(Project, related_name='budgetRequirements', on_delete=models.CASCADE)
 
-class EvaluationAndMonitoring(models.Model):
+class EvaluationAndMonitoring(models.Model): #a9
     EAMID = models.AutoField(primary_key=True)
     projectSummary = models.TextField()
     indicators = models.TextField()
@@ -188,7 +247,7 @@ class EvaluationAndMonitoring(models.Model):
     type = models.CharField(max_length=100)
     project = models.ForeignKey(Project, related_name='evalAndMonitoring', on_delete=models.CASCADE)
 
-class MonitoringPlanAndSchedule(models.Model):
+class MonitoringPlanAndSchedule(models.Model): #a10
     MPASID = models.AutoField(primary_key=True)
     approach = models.TextField()
     dataGatheringStrategy = models.TextField()
